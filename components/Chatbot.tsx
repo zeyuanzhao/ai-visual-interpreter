a0c3542239eac9901e9b646a48873f9f02679aaf
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-10-23 17:25:58 -0400

    layout of messages

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Message from "./Message";
+import { Button, Input } from "@nextui-org/react";
 
 interface MessageType {
   message: string;
@@ -9,10 +10,21 @@ interface MessageType {
 const Chatbot = () => {
   const [messages, setMessages] = useState<MessageType[]>([
     { message: "Hello! How can I help you?", sender: "chatbot" },
+    { message: "Hello! How can I help you?", sender: "user" },
+    { message: "Hello! How can I help you?", sender: "chatbot" },
+    { message: "Hello! How can I help you?", sender: "user" },
+    { message: "Hello! How can I help you?", sender: "chatbot" },
+    { message: "Hello! How can I help you?", sender: "user" },
+    { message: "Hello! How can I help you?", sender: "chatbot" },
+    { message: "Hello! How can I help you?", sender: "user" },
+    { message: "Hello! How can I help you?", sender: "chatbot" },
+    { message: "Hello! How can I help you?", sender: "chatbot" },
+    { message: "Hello! How can I help you?", sender: "chatbot" },
   ]);
+  const [messageInput, setMessageInput] = useState<string>("");
   return (
-    <div className="flex-auto w-full border border-black flex flex-col">
-      <div className="flex-auto border pt-12">
+    <div className="flex-1 w-full border border-black flex flex-col overflow-hidden">
+      <div className="flex-auto border pt-12 pb-4 px-2 space-y-4 overflow-auto">
         {messages.map((message, index) => (
           <Message
             message={message.message}
@@ -21,7 +33,12 @@ const Chatbot = () => {
           />
         ))}
       </div>
-      <div className="h-16 border"></div>
+      <div className="p-2 pb-4 border">
+        <form className="flex flex-row justify-between space-x-2">
+          <Input value={messageInput} onValueChange={setMessageInput} />
+          <Button type="submit">Send</Button>
+        </form>
+      </div>
     </div>
   );
 };
